90c3a1d08d82d31a6f05dd73bf305a1629542ac5
KaptainCS3 <mbiapplegryn@gmail.com>
2022-09-01 15:38:01 +0100

    practice section

--- src/component/ContactCard.js
+++ src/component/ContactCard.js
@@ -1,24 +1,35 @@
-// import React, {useState} from 'react'
-// import image from '../images/avatar.png'
-// const ContactCard = () => {
-//     const [contact, setContact] = useState({
-//         firstName: 'John',
-//         lastName: 'Doe',
-//         phone: '+1 (709) 234-4442',
-//         email: 'mbiapplegryn@gmail.com',
-//         isFavorite: true
-//     })
-//   return (
-//     <div className='contact'>
-//         {/* <article>
-//             <img className='img-contact' src={image} alt='' />
-//             <img src='' alt='' />
-//             <h1>{contact.firstName} {contact.lastName}</h1>
-//             <p>{contact.phone}</p>
-//             <p>{contact.email}</p>
-//         </article> */}
-//     </div>
-//   )
-// }
+import React, {useState} from 'react'
+import image from '../images/avatar.png'
+import Star from './Star'
+const ContactCard = () => {
+    const [contact, setContact] = useState({
+        firstName: 'John',
+        lastName: 'Doe',
+        phone: '+1 (709) 234-4442',
+        email: 'mbiapplegryn@gmail.com',
+        isFavorite: true
+    })
 
-// export default ContactCard
+    function changeState(){
+        setContact(prevContact =>({
+            ...prevContact,
+            isFavorite: !prevContact.isFavorite
+        }))
+    }
+  return (
+    <div className='contact'>
+        <article>
+            <img className='img-contact' src={image} alt='' />
+            <Star
+                isFilled = {contact.isFavorite}
+                handleClick = {changeState}
+            />
+            <h1>{contact.firstName} {contact.lastName}</h1>
+            <p>{contact.phone}</p>
+            <p>{contact.email}</p>
+        </article>
+    </div>
+  )
+}
+
+export default ContactCard
