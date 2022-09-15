8d460d3081cde578392af06cfccf70df69f19948
KaptainCS3 <mbiapplegryn@gmail.com>
2022-09-15 23:24:07 +0100

    controll component[inputs]

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,27 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 const Form = () => {
-  const styles = {
-    label: {
-      color: "#fff",
-      fontSize: "2em",
-    },
-    input: {
-      paddingRight: 10+'px',
-    },
-  };
+  const [formData, seFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comment: "",
+    isFriendly: true,
+  });
+  function handleChange(event) {
+    const {name, type, value, checked} = event.target;
+    seFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
+  console.log(formData);
   return (
     <div>
-      <form style={styles.label}>
-        <label htmlFor="fName" style={styles.input}>
-          First Name
-        </label>
-        <input type="text" placeholder="Enter first name" id="fName" />
+      <form>
+        <label htmlFor="fName">First Name</label>
+        <input
+          type="text"
+          placeholder="Enter first name"
+          id="fName"
+          name="firstName"
+          value={FormData.firstName}
+          onChange={handleChange}
+        />
         <br />
         <br />
-        <label htmlFor="lName" style={styles.input}>
-          Last Name
+        <label htmlFor="lName">Last Name</label>
+        <input
+          type="text"
+          placeholder="Enter last name"
+          id="lName"
+          name="lastName"
+          value={FormData.lastName}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          placeholder="Enter email address"
+          id="email"
+          name="email"
+          value={FormData.email}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <textarea
+          placeholder="Comments"
+          id="cmt"
+          name="comment"
+          value={FormData.comment}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <input
+          type="checkbox"
+          id="checkbox"
+          name="isFriendly"
+          checked={formData.isFriendly}
+          onChange={handleChange}
+        />
+        <label htmlFor="checkbox" className="check">
+          Are you Friendly?
         </label>
-        <input type="text" placeholder="Enter last name" id="lName" />
+        <br />
+        <br />
         <button>Submit</button>
       </form>
     </div>
