5ab36637e0adeba843ce5a0fd8397478d143a0b9
KaptainCS3 <mbiapplegryn@gmail.com>
2022-09-17 18:27:28 +0100

    refactor project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Main from "./component/Main";
+import Nav from "./component/Nav";
+const App = () =>{
+    const [show, setShow] = useState(true);
+    function toggle() {
+      setShow((prevShow) => !prevShow);
+    }
+    return (
+      <div className="container">
+        <Nav />
+        <Main />
+      </div>
+    );
+}
+export default App;
