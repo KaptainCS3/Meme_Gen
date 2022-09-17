221439f5f638db258fdcead96f4f0fd4c12f163e
KaptainCS3 <mbiapplegryn@gmail.com>
2022-09-17 18:30:30 +0100

    Add App component

--- src/index.js
+++ src/index.js
@@ -1,31 +1,4 @@
 import React from "react";
 import ReactDom from "react-dom";
-// import Count from "./component/Count";
-import Main from "./component/Main";
-import Nav from './component/Nav'
-// import IsGoingOut from './component/IsGoingOut'
-// import States from "./component/States";
-// import Test from "./component/Test";
-// import ContactCard from './component/ContactCard'
-// import Form from "./component/Form";
-// import FormPractice from "./component/FormPractice";
-// import data from "./data";
-// import answer from './ternary'
-import './style.css'
-const App = () => {
-  // const isGoingOut = false;
-  return (
-    <div className="container">
-      {/* <FormPractice /> */}
-      {/* <Form /> */}
-      {/* <IsGoingOut /> */}
-      <Nav />
-      <Main />
-      {/* <Test /> */}
-      {/* <ContactCard /> */}
-      {/* <States /> */}
-      {/* <Count /> */}
-    </div>
-  );
-};
+import "./style.css";
 ReactDom.render(<App />, document.getElementById("root"));
